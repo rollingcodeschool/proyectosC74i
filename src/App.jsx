21f150e9dda700b7c0ad9ce29f9a52e0fae4f55e
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-02-27 13:55:07 -0300

    se instalo react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
-import './App.css'
-import Home from './components/Home'
-import Footer from './components/Footer'
-
-
-
-const App=()=> {
+const App = () => {
   return (
     <>
-    <main>
-      <Home></Home>
-    </main>
-    
-    <Footer></Footer>
+      <main>
+        <Home></Home>
+      </main>
+
+      <Footer></Footer>
     </>
-  )
-}
-export default App
+  );
+};
+export default App;
